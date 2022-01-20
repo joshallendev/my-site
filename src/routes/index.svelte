75bb1aed361084d488fe9c966f40fe4ba378fb28
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-01-19 19:50:47 -0800

    Form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -50,7 +50,34 @@
 <Skills />
 <Projects />
 <Contact />
-
+<form method="POST" data-netlify-recaptcha="true" data-netlify="true" class="flex flex-wrap -m-2" name="contact-form">
+  <div class="p-2 w-full">
+  <div>
+      <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+      <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+  </div>
+  </div>
+  <div class="p-2 w-full">
+  <div>
+      <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+      <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+  </div>
+  </div>
+  <div class="p-2 w-full">
+  <div>
+      <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+      <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+  </div>
+  </div>
+  <div data-netlify-recaptcha="true"></div>
+  <div class="p-2 w-full">
+      <button class="flex mx-auto text-richblackfogra border-0 py-2 px-8 focus:outline-none bg-cyan rounded text-lg hover:scale-105" type="submit">Send Message</button>
+  </div>
+  <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+  
+  </div>
+  
+</form>
 <style>
   
 </style>
