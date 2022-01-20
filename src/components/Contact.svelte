4fc0ba7bcf3d3f23d7350cf04ad7c5f8d6e9569a
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-01-19 20:38:01 -0800

    Form

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,5 +1,9 @@
 <script lang=ts>
     import SectionHeader from './SectionHeader.svelte';
+
+    const submitHandler = (event) => {
+
+    }
 </script>
 
 <section id="contact" class="px-10 pt-10 pb-20 bg-platinum w-full scroll-mt-20">
@@ -10,7 +14,7 @@
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">I'd love to help you build something great.</p>
         </div>
         <div class="lg:w-1/2 md:w-2/3 mx-auto">
-        <form method="POST" data-netlify="true" class="flex flex-wrap -m-2" name="contact-form">
+        <form method="POST" data-netlify="true" class="flex flex-wrap -m-2" name="contact">
             <div class="p-2 w-full">
             <div>
                 <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
@@ -35,7 +39,7 @@
             <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
             
             </div>
-            
+            <input type="hidden" name="form-name" value="contact">
         </form>
         </div>
     </div>
