ed27e14f9f1b3f5e9da9cc05aeb0c999d4b8162e
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-01-19 20:24:19 -0800

    Form updates

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -10,7 +10,7 @@
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">I'd love to help you build something great.</p>
         </div>
         <div class="lg:w-1/2 md:w-2/3 mx-auto">
-        <form method="POST" netlify data-netlify-recaptcha="true" class="flex flex-wrap -m-2" name="contact-form">
+        <form method="POST" data-netlify="true" class="flex flex-wrap -m-2" name="contact-form">
             <div class="p-2 w-full">
             <div>
                 <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
@@ -29,7 +29,6 @@
                 <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
             </div>
-            <div data-netlify-recaptcha="true"></div>
             <div class="p-2 w-full">
                 <button class="flex mx-auto text-richblackfogra border-0 py-2 px-8 focus:outline-none bg-cyan rounded text-lg hover:scale-105" type="submit">Send Message</button>
             </div>
