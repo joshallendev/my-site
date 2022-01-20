84ae3bb215e9e12f02220f629ab70b10a0ae6893
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-01-19 21:10:08 -0800

    Form ajax submission

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -4,6 +4,24 @@
     const submitHandler = (event) => {
 
     }
+
+    
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let myForm = document.getElementById("contact-form");
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
 </script>
 
 <section id="contact" class="px-10 pt-10 pb-20 bg-platinum w-full scroll-mt-20">
@@ -14,7 +32,7 @@
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">I'd love to help you build something great.</p>
         </div>
         <div class="lg:w-1/2 md:w-2/3 mx-auto">
-        <form method="POST" data-netlify="true" class="flex flex-wrap -m-2" name="contact">
+        <form method="POST" data-netlify="true" id="contact-form" class="flex flex-wrap -m-2" name="contact">
             <div class="p-2 w-full">
             <div>
                 <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
